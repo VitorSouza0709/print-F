8c78aa4c704ea7c3fbf1d19cdb84eeec68cdbc0b
Vitor <169068391+VitorSouza0709@users.noreply.github.com>
2024-09-02 12:13:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+  public static void main(String[] args) {
+    
+    
+    String nome = "vitor";
+    int dia = 7;
+    int mes = 9;
+    int ano = 2008;
+    double pi = 3.14159265359;
+
+    System.out.printf(" minha data de nascimento é: %d/%d/%d", dia, mes, ano);
+
+    System.out.printf(" o valor de pi é: %f.4f  " ,pi);
+  }
+
+
+}
